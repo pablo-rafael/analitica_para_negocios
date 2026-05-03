74e22d5f68e2cd98003adc1ab869765e1e35a535
pablo-rafael <prfoose@gmail.com>
2026-05-03 20:47:46 -0300

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-# import matplotlib
-# matplotlib.use('Agg')
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.datasets import fetch_california_housing
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
